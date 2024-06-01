5d4bac1cbdf274fafd50803a9147556b4ac23d07
Mohamad Samardeh Haghighi Fard <samardehmohamad@gmail.com>
2024-06-01 11:59:08 +0100

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -1,11 +1,11 @@
 def get_telegram_token() -> str:
-    return Your Telegram Token
+    return "6254647714:AAEc54rwHusIZ0DR0-krGcAkzGvfqaUXroA"
 
 def get_twitch_token() -> str:
-    return Your Twitch API Token (Bearer)
+    return "34bw7fhtoiyl8f4h9qhzkmft19b645"
 
 def get_client_id() -> str:
-    return Your ClientID
+    return "1af8lhn20rwiuurv3zdu295zcdwako"
 
 def get_chat_id() -> int:
-    return Your ChatID
+    return 123456789  # Replace this with your actual chat ID
